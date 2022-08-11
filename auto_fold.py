9607b4897c0906d08b8fcd165f4a10a9e8b81192
espen21 <31931020+espen21@users.noreply.github.com>
2022-08-11 11:45:16 +0200

    funkar bra nu

--- auto_fold.py
+++ auto_fold.py
@@ -1,11 +1,11 @@
 import win32gui,win32api,win32con
 import time
 import keyboard
-def send_click(handle):
+def send_click(handle,press):
     win32gui.SetActiveWindow(handle)
-    keyboard.press_and_release("ctrl+left")
-    
-    print("pressed ")    
+    if press: keyboard.press("ctrl+left")
+    else: keyboard.release("ctrl+left")
+    print(win32gui.GetWindowText(handle),press)
 state_left = win32api.GetKeyState(0x06)  # Left button down = 0 or 1. Button up = -127 or -128
 
 
@@ -16,9 +16,8 @@ while True:
     a = win32api.GetKeyState(0x06)
     if a != state_left:  # Button state changed
         state_left = a
-        if a < 0:
-          
-            pass
-        elif "- PL Omaha -" in name or "NLH" in name or "- NL Hold" in name or "Rush & Cash" in name:
-            send_click(handle)
+        if a < 0 and ("- PL Omaha -" in name or "NLH" in name or "- NL Hold" in name or "Rush & Cash" in name or "Spin & Gold" in name):
+            send_click(handle,True)
+        else:
+            send_click(handle,False)
     time.sleep(0.1)
